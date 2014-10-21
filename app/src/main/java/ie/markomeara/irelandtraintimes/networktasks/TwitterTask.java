7e57699fbbbe1b46a81aa5b9c327b0051bef6efa
markomeara <omearamark200@gmail.com>
2014-10-21 12:41:02 +0100

    Task for getting Irish Rail Twitter feed

---
+++ app/src/main/java/ie/markomeara/irelandtraintimes/networktasks/TwitterTask.java
@@ -0,0 +1,13 @@
+package ie.markomeara.irelandtraintimes.networktasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Mark on 21/10/2014.
+ */
+public class TwitterTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
