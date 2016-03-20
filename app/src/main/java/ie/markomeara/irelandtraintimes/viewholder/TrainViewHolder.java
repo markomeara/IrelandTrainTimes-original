6fd39f53241a6015f9e91d3c443ebe4490ce8759
markomeara <markomeara@users.noreply.github.com>
2016-03-20 00:21:02 +0000

    Extracted TrainViewHolder into separate class

---
+++ app/src/main/java/ie/markomeara/irelandtraintimes/viewholder/TrainViewHolder.java
@@ -0,0 +1,132 @@
+package ie.markomeara.irelandtraintimes.viewholder;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ie.markomeara.irelandtraintimes.R;
+import ie.markomeara.irelandtraintimes.fragments.StationNextTrainsFragment;
+import ie.markomeara.irelandtraintimes.model.Train;
+
+/**
+ * Created by markomeara on 20/03/2016.
+ */
+public class TrainViewHolder extends RecyclerView.ViewHolder {
+
+    private static final int NO_COLOR = -1;
+
+    private final View mTrainItem;
+    @Bind(R.id.reminderButton)
+    View mReminderBtn;
+    @Bind(R.id.traindue_dest_TV)
+    TextView trainDest_TV;
+    @Bind(R.id.traindue_mins_TV)
+    TextView trainDueMins_TV;
+    @Bind(R.id.traindue_delay_TV)
+    TextView trainDelayMins_TV;
+    @Bind(R.id.traindue_time_TV)
+    TextView trainDueTime_TV;
+
+    private final StationNextTrainsFragment mListener;
+
+    public TrainViewHolder(View view, StationNextTrainsFragment listener) {
+        super(view);
+        mTrainItem = view;
+        ButterKnife.bind(this, view);
+        mListener = listener;
+        createOnClickListeners();
+    }
+
+    public View getView(){
+        return mTrainItem;
+    }
+
+    private void createOnClickListeners(){
+        mTrainItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = getAdapterPosition();
+                if(adapterPosition != RecyclerView.NO_POSITION){
+                    mListener.onTrainSelected(getAdapterPosition());
+                }
+            }
+        });
+
+        mReminderBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = getAdapterPosition();
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    // TODO Something
+                }
+            }
+        }));
+    }
+    public void setTrainDestination(String destination){
+        trainDest_TV.setText(destination);
+    }
+
+    public void setTrainDueMins(int trainDueMins){
+        trainDueMins_TV.setText(Integer.toString(trainDueMins) + " mins");
+    }
+
+    public void setTrainDelayMins(int trainDelayMins){
+        String trainDelayMinsDisp = formatDelayMinsToString(trainDelayMins);
+        trainDelayMins_TV.setText(trainDelayMinsDisp);
+
+        int delayColor = colorForDelayMins(trainDelayMins);
+        if(delayColor != NO_COLOR){
+            trainDelayMins_TV.setTextColor(delayColor);
+            trainDueMins_TV.setTextColor(delayColor);
+        }
+    }
+
+    public void setTrainDueTime(String trainDueTime){
+        trainDueTime_TV.setText(trainDueTime);
+    }
+
+    public void doSomethingWithReminderBtn(){
+        // TODO Something
+    }
+
+    private String formatDelayMinsToString(int delayMins){
+        StringBuilder delayMinsDisplay = new StringBuilder();
+
+        if(delayMins != 0){
+            String sign = "";
+            // Negative delay will already have a minus sign from API
+            if(delayMins > 0){
+                sign = "+";
+            }
+            delayMinsDisplay.append("(");
+            delayMinsDisplay.append(sign);
+            delayMinsDisplay.append(delayMins);
+            delayMinsDisplay.append(")");
+        }
+
+        return delayMinsDisplay.toString();
+    }
+
+    private int colorForDelayMins(int delayMins){
+        Context ctx = mListener.getActivity();
+        int colorId = NO_COLOR;
+        if(delayMins < 0){
+            // Train is early
+            colorId = ctx.getResources().getColor(R.color.irishrailgreen);
+        }
+        else if(delayMins > 0 &&  delayMins < Train.MAJORDELAY_MINS){
+            // Train has minor delay
+            colorId = ctx.getResources().getColor(R.color.minordelay);
+        }
+        else if(delayMins >= Train.MAJORDELAY_MINS){
+            // Train has major delay
+            colorId = ctx.getResources().getColor(R.color.majordelay);
+        }
+        return colorId;
+
+    }
+
+}
--- app/src/main/java/ie/markomeara/irelandtraintimes/views/adapters/TrainsDueRecyclerViewAdapter.java
+++ app/src/main/java/ie/markomeara/irelandtraintimes/views/adapters/TrainsDueRecyclerViewAdapter.java
@@ -16,6 +16,7 @@ import ie.markomeara.irelandtraintimes.fragments.StationNextTrainsFragment;
 import ie.markomeara.irelandtraintimes.model.Train;
 import ie.markomeara.irelandtraintimes.model.TrainListHeader;
 import ie.markomeara.irelandtraintimes.model.TrainListItem;
+import ie.markomeara.irelandtraintimes.viewholder.TrainViewHolder;
 
 /**
  * Created by markomeara on 08/11/2015.
@@ -24,7 +25,6 @@ public class TrainsDueRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerV
 
     private List<TrainListItem> mListItems;
     private StationNextTrainsFragment mParentFragment;
-    private static final int NO_COLOR = -1;
 
     public TrainsDueRecyclerViewAdapter(List<TrainListItem> trains, StationNextTrainsFragment parentFragment) {
         this.mListItems = trains;
@@ -112,118 +112,4 @@ public class TrainsDueRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerV
         return mListItems.get(position).getViewType();
     }
 
-    public static class TrainViewHolder extends RecyclerView.ViewHolder {
-
-        private final View mTrainItem;
-        @Bind(R.id.reminderButton)
-        View mReminderBtn;
-        @Bind(R.id.traindue_dest_TV)
-        TextView trainDest_TV;
-        @Bind(R.id.traindue_mins_TV)
-        TextView trainDueMins_TV;
-        @Bind(R.id.traindue_delay_TV)
-        TextView trainDelayMins_TV;
-        @Bind(R.id.traindue_time_TV)
-        TextView trainDueTime_TV;
-
-        private final StationNextTrainsFragment mListener;
-
-        public TrainViewHolder(View view, StationNextTrainsFragment listener) {
-            super(view);
-            mTrainItem = view;
-            ButterKnife.bind(this, view);
-            mListener = listener;
-            createOnClickListeners();
-        }
-
-        public View getView(){
-            return mTrainItem;
-        }
-
-        private void createOnClickListeners(){
-            mTrainItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int adapterPosition = getAdapterPosition();
-                    if(adapterPosition != RecyclerView.NO_POSITION){
-                        mListener.onTrainSelected(getAdapterPosition());
-                    }
-                }
-            });
-
-            mReminderBtn.setOnClickListener((new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int adapterPosition = getAdapterPosition();
-                    if (adapterPosition != RecyclerView.NO_POSITION) {
-                        // TODO Something
-                    }
-                }
-            }));
-        }
-        public void setTrainDestination(String destination){
-            trainDest_TV.setText(destination);
-        }
-
-        public void setTrainDueMins(int trainDueMins){
-            trainDueMins_TV.setText(Integer.toString(trainDueMins) + " mins");
-        }
-
-        public void setTrainDelayMins(int trainDelayMins){
-            String trainDelayMinsDisp = formatDelayMinsToString(trainDelayMins);
-            trainDelayMins_TV.setText(trainDelayMinsDisp);
-
-            int delayColor = colorForDelayMins(trainDelayMins);
-            if(delayColor != NO_COLOR){
-                trainDelayMins_TV.setTextColor(delayColor);
-                trainDueMins_TV.setTextColor(delayColor);
-            }
-        }
-
-        public void setTrainDueTime(String trainDueTime){
-            trainDueTime_TV.setText(trainDueTime);
-        }
-
-        public void doSomethingWithReminderBtn(){
-            // TODO Something
-        }
-
-        private String formatDelayMinsToString(int delayMins){
-            StringBuilder delayMinsDisplay = new StringBuilder();
-
-            if(delayMins != 0){
-                String sign = "";
-                // Negative delay will already have a minus sign from API
-                if(delayMins > 0){
-                    sign = "+";
-                }
-                delayMinsDisplay.append("(");
-                delayMinsDisplay.append(sign);
-                delayMinsDisplay.append(delayMins);
-                delayMinsDisplay.append(")");
-            }
-
-            return delayMinsDisplay.toString();
-        }
-
-        private int colorForDelayMins(int delayMins){
-            Context ctx = mListener.getActivity();
-            int colorId = NO_COLOR;
-            if(delayMins < 0){
-                // Train is early
-                colorId = ctx.getResources().getColor(R.color.irishrailgreen);
-            }
-            else if(delayMins > 0 &&  delayMins < Train.MAJORDELAY_MINS){
-                // Train has minor delay
-                colorId = ctx.getResources().getColor(R.color.minordelay);
-            }
-            else if(delayMins >= Train.MAJORDELAY_MINS){
-                // Train has major delay
-                colorId = ctx.getResources().getColor(R.color.majordelay);
-            }
-            return colorId;
-
-        }
-
-    }
 }
